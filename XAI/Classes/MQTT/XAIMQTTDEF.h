e707cc40275efe57ea58f707dd24044e1d162233
touchhy <alibaba@alibabatekiMacBook-Air.local>
2014-04-04 13:21:13 +0800

    ..........

--- XAI/Classes/MQTT/XAIMQTTDEF.h
+++ XAI/Classes/MQTT/XAIMQTTDEF.h
@@ -12,7 +12,7 @@
 // Include any system framework and library headers here that should be included in all compilation units.
 // You will also need to set the Prefix Header build setting of one or more of your targets to reference this file.
 
-typedef XAI_PKT_TYPE(int, PKT_TYPE) {
+typedef enum XAI_PKT_TYPE{
     
     
     XAI_PKT_TYPE_HELLO	 = 0,	//Hello报文
@@ -23,9 +23,9 @@ typedef XAI_PKT_TYPE(int, PKT_TYPE) {
     XAI_PKT_TYPE_STATUS = 5,	//状态表答复报文
     XAI_PKT_FLAG_ACK = 0x80,  //	FLAG最高位为1表示此报文是一个ACK报文
     
-};
+}XAI_PKT_TYPE;
     
-typedef XAI_DATA_TYPE(int,XAI_DATA_TYPE){
+typedef  enum XAI_DATA_TYPE{
     
    
     XAI_DATA_TYPE_ASCII_TEXT = 0,//	字符串数据
@@ -39,7 +39,31 @@ typedef XAI_DATA_TYPE(int,XAI_DATA_TYPE){
     XAI_DATA_TYPE_BIN_LUID = 8, //	本地唯一编号
         
         
-};
+}XAI_DATA_TYPE;
+    
+    
+    //大小
+#define  _XPPS_N_FROM_GUID  12
+#define  _XPPS_N_TO_GUID    12
+#define  _XPPS_N_FLAG    1
+#define  _XPPS_N_MSGID   2
+#define  _XPPS_N_MAGIC_NUMBER  2
+#define  _XPPS_N_LENGTH       2
+#define  _XPPS_N_FIXED_ALL  (12+12+1+2+2+2)
+    
+    //开始与结束位置  0 表示第一位
+#define  _XPP_N_FROM_GUID_START  0
+#define  _XPP_N_FROM_GUID_END  11
+#define  _XPP_N_TO_GUID_START   12
+#define  _XPP_N_TO_GUID_END    23
+#define  _XPP_N_FLAG_START    24
+#define  _XPP_N_FLAG_END      24
+#define  _XPP_N_MSGID_START   25
+#define  _XPP_N_MSGID_END   26
+#define  _XPP_N_MAGIC_NUMBER_START  27
+#define  _XPP_N_MAGIC_NUMBER_END  28
+#define  _XPP_N_LENGTH_START        29
+#define  _XPP_N_LENGTH_END        30
 
     
 
